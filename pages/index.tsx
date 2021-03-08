2c11a94c260e36f93beb37c792b9d94389c0d2e0
Dimitrije Jankovic <dimitrije_jankovic@outlook.com>
2021-03-08 23:28:35 +0100

    routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      
+      <Link prefetch={true} href={{
+        pathname: '/[username]',
+        query: { username: 'dika94' }
+      }}>
+        <a>Dika profile</a>
+      </Link>
     </div>
   )
 }
