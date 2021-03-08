6f3eddbf39a03f23de5e987b2f698d6157b49b58
Dimitrije Jankovic <dimitrije_jankovic@outlook.com>
2021-03-08 23:56:39 +0100

    react hot toast added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,9 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-import Loader from '../components/Loader' 
-
 export default function Home() {
   return (
     <div>
-
 
       {/*<Link prefetch={true} href={{
         pathname: '/[username]',
